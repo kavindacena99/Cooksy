241fa0d2758c20ba598b9216228c3499d24bb57f
kavinda <kavindach4@gmail.com>
2025-05-10 19:24:25 +0530

    OK

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View,Text } from 'react-native';
+import './css/global.css';
+
+function App(){
+  return(
+    <View>
+      <Text style={{marginTop:30,marginLeft:30,fontSize:35}}  className="text-black text-lg font-bold">Welcome</Text>
+    </View>
+  );
+}
+
+export default App;
